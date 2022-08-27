34ea9cf247a7c7c6ba50f6a1ec0eae01c10afb5d
OlegYelis <73547498+OlegYelis@users.noreply.github.com>
2022-08-27 23:12:07 +0300

    Task 3 || Done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+const inputDelayEl = document.querySelector('input[name="delay"]');
+const inputStepEl = document.querySelector('input[name="step"]');
+const inputAmountEl = document.querySelector('input[name="amount"]');
+
+let totalDelay = Number(inputDelayEl.value);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return (promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+      reject({ position, delay });
+    }, totalDelay);
+  }));
 }
+
+const totalPromises = () => {
+  for (let i = 0; i < Number(inputAmountEl.value); i += 1) {
+    createPromise(i + 1, (totalDelay += Number(inputStepEl.value)))
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+};
+
+formEl.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+  setTimeout(totalPromises, Number(inputDelayEl.value));
+
+  totalDelay = Number(inputDelayEl.value);
+});
